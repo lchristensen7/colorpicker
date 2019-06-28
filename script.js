be37a400356b515be1bb15c6974f1789807b24a2
Lariah Christensen <lariah_14@hotmail.com>
2019-06-28 01:54:54 -0600

    part 2

--- script.js
+++ script.js
@@ -1,25 +1,46 @@
-window.onload = function () {
+window.onload = function() {
 	initColorPicker();
 };
 
 function initColorPicker() {
-	let colorBox = document.getElementById(`color-box`);
+	let colorBox = document.getElementById('color-box');
 	let rgb = {
-		red: document.getElementById("red)
-		green: document.getElementById("green")
-		blue: document.getElementById("blue)
+		red: document.getElementById('red'),
+		green: document.getElementById('green'),
+		blue: document.getElementById('blue'),
 	};
-	setColorPickerEventListeners(colorBox, rgb);
+	let colorPickers = document.getElementsByClassName('picker');
+	setColorPickerEventListeners(colorBox, rgb, colorPickers);
 }
 
-function setColorPickerEventListeners(colorBox, rgb) {
-	rgb.red.addEventListener('change', () => {
-		console.log("Red value:", rgb.red.value);
-	});
-	rgb.green.addEventListener('change', () => {
-		console.log("Green value:", rgb.green.value);
-	});
-	rgb.blue.addEventListener('change', () => {
-		console.log("Blue value:", rgb.blue.value);
-	});
+function setColorPickerEventListeners(element, colors, pickerElements) {
+	let pickerLength = pickerElements.length;
+	for (let i = 0; i < pickerLength; i++) {
+		pickerElements[i].addEventListener('change', () => {
+			let red = colors.red.value;
+			let green = colors.green.value;
+			let blue = colors.blue.value;
+			setElementBGColor(element, red, green, blue);
+			setDisplayValues(red, green, blue);
+		});
+
+	}
+
+}
+
+function setElementBGColor(bgElement, red, green, blue) {
+	let rgbval = [red, green, blue].join(',');
+	bgElement.style.backgroundColor = "rgb(" + rgbval + ")";
+
+}
+
+function setDisplayValues(red, green, blue) {
+	let redVal = document.getElementById('redVal');
+	let greenVal = document.getElementById('greenVal');
+	let blueVal = document.getElementById('blueVal');
+
+	redVal.innerText = red;
+	greenVal.innerText = green;
+	blueVal.innerText = blue;
+
 }
